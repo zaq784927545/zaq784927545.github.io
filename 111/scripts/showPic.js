82e2dd99f467badd4413159ea4121d31ffa857f8
zaq784927545 <38269844+zaq784927545@users.noreply.github.com>
2018-04-18 03:36:40 +0000

    Update showPic.js

--- 111/scripts/showPic.js
+++ 111/scripts/showPic.js
@@ -2,6 +2,8 @@ function  showPic(whichpic) {
  var source = whichpic.getAttribute("href");
  var placeholder = document.getElementById("placeholder");
  placeholder.setAttribute("src",source);
+ var text = whichpic.getAttribute("title");
+ var description = document.getElementById("description");
 }
 function countBodyChildren(){
  var body_element = document.getElementsByTagName("body")[0];
