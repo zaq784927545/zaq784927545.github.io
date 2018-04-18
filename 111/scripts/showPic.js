c2013e6eca320da6fbe70ffee97ffca0972e91a3
zaq784927545 <38269844+zaq784927545@users.noreply.github.com>
2018-04-18 03:28:57 +0000

    Update showPic.js

--- 111/scripts/showPic.js
+++ 111/scripts/showPic.js
@@ -3,3 +3,8 @@ function  showPic(whichpic) {
  var placeholder = document.getElementById("placeholder");
  placeholder.setAttribute("src",source);
 }
+function countBodyChildren(){
+ var body_element = document.getElementsByTagName("body")[0];
+ alert (body_element.childNodes.length);
+}
+window.onload = countBodyChildren;
