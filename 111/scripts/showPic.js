02bffbb9a30b9f1e80e3443306b97d26980a9acc
zaq784927545 <38269844+zaq784927545@users.noreply.github.com>
2018-05-02 12:49:56 +0800

    Update showPic.js

--- 111/scripts/showPic.js
+++ 111/scripts/showPic.js
@@ -1,15 +1,57 @@
-function  showPic(whichpic) {
- var source = whichpic.getAttribute("href");
- var placeholder = document.getElementById("placeholder");
- placeholder.setAttribute("src",source);
- var text = whichpic.getAttribute("title");
- var description = document.getElementById("description");
-window.onload = countBodyChildren;
-function showPic(whichpic){
- var source = whichpic.getAttribute("href");
- var placeholder = document.getElmentById("placeholder");
- placeholder.setAttribute("src",source);
- var text = whichpic.getAttribute("title");
- var description = document.getElementById("description");
- description.firstChild.nodeValue = text;
+function prepareGallery()
+{
+     if(!documet.getElementsByTagName)
+		return false;
+     if(!documet.getElementById)
+		return false;
+     if(!documet.getElementById("imagegallery"))
+		return false;
+	var gallery = document.getElementById("imagegallery");
+	var links=gallery.getElementsByTagName("a");
+	for( var i=0;i<links.length;i++)
+	{
+		links[i].onclick = function()
+			{		
+				return showPic(this) ? false : true;
+			}
+	}
 }
+ <!--  add -->
+	function addLoadEvent(func)
+		{	
+			var oldonload = window.omload;
+			if(typeof window.onload != 'function')
+			{
+				window.onload = func;
+			}
+			else
+			{
+				window.onload = function()
+				{
+					oldonload();
+					func();
+				}
+			}
+		}
+function showPic(whicp)
+{
+	if(!document.getElementById("zwf"))
+	return false;
+	var soure = 	whicp.getAttribute("href");
+	var pla = 	document.getElementById("zwf");
+	if(pla.nodeName != "IMG" )
+	return false;
+	pla.setAttribute("src",soure);	
+	if(document.getElementById("des"))
+{
+			// 显示提示文字
+	var text = whicp.getAttribute("title") ? whicp.getAttribute("title") :"";
+	var des = document.getElementById("des");
+	if(des.firstChild.nodeType==3)
+{
+	des.firstChild.nodeValue = text;			
+}
+}
+ return  true;
+}
+
