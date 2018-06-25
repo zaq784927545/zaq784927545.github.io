c4eff97dc6bc05f541a79faf5130833fb4c6bab2
zaq784927545 <38269844+zaq784927545@users.noreply.github.com>
2018-06-25 20:27:38 +0800

    Update showPic.js

--- showPic.js
+++ showPic.js
@@ -1,87 +1,48 @@
-function prepareGallery()
-{
-
-     if(!documet.getElementsByTagName)
-		return false;
-     if(!documet.getElementById)
-		return false;
-     if(!documet.getElementById("imagegallery"))
-		return false;
-	var gallery = document.getElementById("imagegallery");
-	var links=gallery.getElementsByTagName("a");
-
-	for( var i=0;i<links.length;i++)
-	{
-		links[i].onclick = function()
-			{
-				
-				return showPic(this) ? false : true;
-			}
-	}
-
-
-}
- <!--  add -->
-	function addLoadEvent(func)
-		{	
-			var oldonload = window.omload;
-			if(typeof window.onload != 'function')
-			{
-				window.onload = func;
-			}
-			else
-			{
-				window.onload = function()
-				{
-					oldonload();
-					func();
-				}
-			}
-		}
-
-
-
-function showPic(whicp)
-{
-		if(!document.getElementById("zwf"))
-			return false;
-		
-
-
-	var soure = 	whicp.getAttribute("href");
-	var pla = 	document.getElementById("zwf");
-
-	if(pla.nodeName != "IMG" )
-		return false;
-
-
-	pla.setAttribute("src",soure);	
-		
-	if(document.getElementById("des"))
-
-	{
-			// 显示提示文字
-	var text = whicp.getAttribute("title") ? whicp.getAttribute("title") :"";
-	var des = document.getElementById("des");
-	if(des.firstChild.nodeType==3)
-		{
-			des.firstChild.nodeValue = text;			
-		}
-	
-
-	}
-
-
-	return  true;
-	
-	
+function showPic(whichpic) {
+  if (!document.getElementById("placeholder")) return true;
+  var source = whichpic.getAttribute("href");
+  var placeholder = document.getElementById("placeholder");
+  placeholder.setAttribute("src",source);
+  if (!document.getElementById("description")) return false;
+  if (whichpic.getAttribute("title")) {
+    var text = whichpic.getAttribute("title");
+  } else {
+    var text = "";
+  }
+  var description = document.getElementById("description");
+  if (description.firstChild.nodeType == 3) {
+    description.firstChild.nodeValue = text;
+  }
+  return false;
 }
 
+function prepareGallery() {
+  if (!document.getElementsByTagName) return false;
+  if (!document.getElementById) return false;
+  if (!document.getElementById("imagegallery")) return false;
+  var gallery = document.getElementById("imagegallery");
+  var links = gallery.getElementsByTagName("a");
+  for ( var i=0; i < links.length; i++) {
+    links[i].onclick = function() {
+      return showPic(this);
+	}
+    links[i].onkeypress = links[i].onclick;
+  }
+}
 
+function addLoadEvent(func) {
+  var oldonload = window.onload;
+  if (typeof window.onload != 'function') {
+    window.onload = func;
+  } else {
+    window.onload = function() {
+      oldonload();
+      func();
+    }
+  }
+}
 
-
-
-
+addLoadEvent(prepareGallery);
 
 
 
